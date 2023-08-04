ca1eccc2de25842eb2a33eb6371aa222093e4db8
PTC-TCOM02\divine.sackitey <divine.sackitey@petraonline.com>
2023-08-04 15:06:43 +0000

    displayed table for books in store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import AddBook from "./components/AddBook";
 import Header from "./components/Header";
+import ViewStore from "./components/ViewStore";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <AddBook />
+      <ViewStore />
     </div>
   );
 }
--- src/components/ViewStore.jsx
+++ src/components/ViewStore.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import { Container, Row, Table } from 'react-bootstrap'
 
 const ViewStore = () => {
   return (
-    <div>ViewStore</div>
+    <Container style={{marginTop: '20px'}}>
+        <button style={{marginBottom: '10px'}}>Refresh</button>
+        <Row>
+            <Table striped bordered hover size="sm">
+                <thead>
+                    <tr style={{textAlign: 'center'}}>
+                        <th>#</th>
+                        <th>Book Title</th>
+                        <th>Book Author</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr style={{textAlign: 'center'}}>
+                        <td>1</td>
+                        <td>Mark</td>
+                        <td>Otto</td>
+                        <td>
+                            <button style={{backgroundColor: 'gray', color: 'white'}}>Edit</button>
+                            <button style={{backgroundColor: 'red', color: 'white'}}>Delete</button>
+                        </td>
+                    </tr>
+                    <tr style={{textAlign: 'center'}}>
+                        <td>2</td>
+                        <td>Jacob</td>
+                        <td>Thornton</td>
+                        <td>
+                            <button style={{backgroundColor: 'gray', color: 'white'}}>Edit</button>
+                            <button style={{backgroundColor: 'red', color: 'white'}}>Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+        </Row>
+    </Container>
   )
 }
 
